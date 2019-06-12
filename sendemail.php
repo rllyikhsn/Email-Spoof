f2a9e1b2c927d6b9faf9f5218d72379e860cfa14
Rully Ikhsan <angledark68@gmail.com>
2019-06-12 23:35:25 +0700

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -1,22 +1,58 @@
 <?php
 require('csv.php');
 
-$headers =  'MIME-Version: 1.0' . "\r\n"; 
-$headers .= 'From: Your name <info@address.com>' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
-
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 /*
-for ($x = 1; $x <  sizeof($the_big_array); $x++){
-	$email = $the_big_array[$x][3];
-	$message = $the_big_array[$x][2];
-	echo '<br>';
-	
-}
+
 */
-$hostname = '{imap.gmail.com:143/imap/ssl/novalidate-cert}'; 
-$username = 'formpengisiancsf@gmail.com'; $password = 'Csfjuni2019'; 
-$inbox = imap_open($hostname,$username,$password) or die('Cannot connect to Gmail: ' . imap_last_error());
 
-print_r(imap_errors());
+require 'vendor/autoload.php';
 
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+    $mail->isSMTP();                                            // Set mailer to use SMTP
+    $mail->Host       = 'ssl://smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'datakhusus40@gmail.com';                     // SMTP username
+    $mail->Password   = '@Frank123';                               // SMTP password
+    $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+
+    //Recipients
+	$mail->setFrom('datakhusus40@gmail.com', 'Creator');
+	for ($x = 1; $x <  sizeof($the_big_array); $x++){
+		$email = $the_big_array[$x][3];
+		$mail->addAddress($email,$email);
+		$mail->Subject = 'Here is the subject';
+		$message = $the_big_array[$x][2];
+		$mail->Body    = $message;	
+    	echo 'Message has been sent';	
+	}
+	
+	$mail->send();
+    //$mail->addAddress('angledark68@gmail.com', 'Rully Ikhsan');     // Add a recipient
+    /*$mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+	$mail->addBCC('bcc@example.com');
+	*/
+    // Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    //$mail->isHTML(true);                                  // Set email format to HTML
+    //$mail->Subject = 'Here is the subject';
+    //$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+
+    //$mail->send();
+    //echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 ?>
