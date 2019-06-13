d84416919527723d7b5f2aa7f36e42f673193653
Rully Ikhsan <angledark68@gmail.com>
2019-06-13 15:55:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php include 'csv.php'; ?>
 <html>
 <head>
+<style> 
+textarea#test {
+  resize: none;
+}
+</style>
 <title>Form Send Email</title>
 </head>
 <body>
@@ -11,31 +16,49 @@
 	<input type="submit" name="delete" value="Delete">
 </form>
 
-<table style="width:100%" border="1">
-  <tr>
-	<th>No</th>
-    <th>Name</th>
-    <th>Email</th> 
-    <th>Message</th>
-  </tr>
-  <?php 
-  $x = 1;
-  while ($x < sizeof($the_big_array) ){ ?>
-  <tr>
-	<td><?php echo $x; ?></td>
-	<td><?php echo $the_big_array[$x][1]; ?></td>
-	<td><?php echo $the_big_array[$x][3]; ?></td>
-	<td><?php echo $the_big_array[$x][2]; ?></td>
-  </tr>
-  <?php $x++;} ?>
-</table>
+
 
 <br>
 <div align="center">
 <form action="sendemail.php"method="POST">
-	<button type="submit">Send Email</button>
+  <table style="width:100%" border="1">
+    <tr>
+    <th>No</th>
+      <th>Name</th>
+      <th>Email</th> 
+      <th>Message</th>
+      <th>BCC</th>
+    </tr>
+    <?php 
+    $x = 1;
+    while ($x < sizeof($the_big_array) ){ ?>
+    <tr>
+    <td><?php echo $x; ?></td>
+    <td><?php echo $the_big_array[$x][1]; ?></td>
+    <td><?php echo $the_big_array[$x][3]; ?></td>
+    <td><?php echo $the_big_array[$x][2]; ?></td>
+    <td>
+      <textarea id = "test" type="text" name="bcc<?php echo $x; ?>" style="width:100%" rows="5" ></textarea>
+    </tr>
+    <?php $x++;} ?>
+  </table>
+  <input type="submit" name="multi_send" value="Send Email ke semua data">
 </form>
 <div>
+<br>
+<div>
+  <form action="sendemail.php" method="post">
+      Masukkan Alamat penerima :<br> 
+      <input type="text" name="penerima"><br>
+      Masukkan Subjek : <br>
+      <input type="text" name="subjek"><br>
+      Masukkan BCC : <br>
+      <textarea type="text" rows="4" cols="25" name="bcc"></textarea><br>
+      Masukkan Isi Pesan : <br>
+      <textarea type="text" rows="4" cols="25" name="isi_pesan"></textarea><br>
+      <input type="submit" name="single_send" value="Kirim Pesan Satu orang" row="10">
+  </form>
+</div>
 
 </body>
 
