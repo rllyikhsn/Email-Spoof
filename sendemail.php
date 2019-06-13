b3618fead065a5d428f355f855867f463133d9ef
Rully Ikhsan <angledark68@gmail.com>
2019-06-13 15:55:00 +0700

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -3,9 +3,6 @@ require('csv.php');
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-/*
-
-*/
 
 require 'vendor/autoload.php';
 
@@ -24,37 +21,76 @@ try {
     $mail->Port       = 465;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('datakhusus40@gmail.com', 'CS Finance');
-	for ($x = 1; $x <  sizeof($the_big_array); $x++){
-		$email = $the_big_array[$x][3];
-		$mail->addAddress($email,$the_big_array[$x][1]);
-		$mail->Subject = 'Here is the subject';
-		$message = $the_big_array[$x][1];
-        $mail->Body    = $message;	
-        echo $mail->Body;
-        echo 'Message has been sent';
-        $mail->send();
-	}
-	
-    //$mail->addAddress('xxx@gmail.com', 'Rully Ikhsan');     // Add a recipient
-    /*$mail->addAddress('ellen@example.com');               // Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-	$mail->addBCC('bcc@example.com');
-	*/
-    // Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+    $mail->setFrom('formpengisiancsf@gmail.com', 'CS Finance');
+    
+    //kirim sesuai button
+    if(isset($_POST['multi_send'])){
+        for ($x = 1; $x <  sizeof($the_big_array); $x++){
+            $email = $the_big_array[$x][3];
+            $mail->addAddress($email,$email);
+            $mail->Subject = 'Here is the subject';
+            $message = $the_big_array[$x][1];
+            $mail->Body    = $message;
+            $bcc = $_POST['bcc'];
+            //$emailbcc = explode(';',$bcc);
+            echo '<br>';	
+            echo $mail->Body;
+            echo '<br>';
+            echo $email;
+            echo '<br>';
+            echo $bcc;
+            echo '<br>';
+            //echo (print_r($emailbcc));
+            echo '<br>';
+            echo '------------------------------';
+            $mail->addBCC('bcc@example.com');
+            //$mail->send();
+        }
+    }
 
-    // Content
-    //$mail->isHTML(true);                                  // Set email format to HTML
-    //$mail->Subject = 'Here is the subject';
-    //$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    if(isset($_POST['single_send'])){
+        $penerima = $_POST['penerima'];
+        $subjek = $_POST['subjek'];
+        $pesan = $_POST['isi_pesan'];
+        $bcc = $_POST['bcc'];
+        $array = array($bcc);
+        
 
-    //$mail->send();
-    //echo 'Message has been sent';
-
+        $mail->addAddress($penerima,$penerima);
+        $mail->addBCC($penerima);
+        $mail->addCC($penerima);
+        $mail->addReplyTo('formpengisiancsf@gmail.com', 'Information');
+        $mail->Subject = $subjek;
+        $mail->Body    = $pesan;
+        try{
+            $mail->send();
+            /*echo '<script type="text/javascript">';
+            echo 'alert("Pesan berhasil dikirim !")';
+            echo '</script>';
+            echo "<script>location='./';</script>";*/
+        }catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+        
+    }
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+
+
+
+
+
+
+// Attachments
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+// Content
+//$mail->isHTML(true);                                  // Set email format to HTML
+//$mail->Subject = 'Here is the subject';
+//$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+
+//$mail->send();
+//echo 'Message has been sent';
 ?>
